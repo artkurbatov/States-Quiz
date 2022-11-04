d4e8e198faf94dde94c104d3f4501dbbaab53a73
KurbatovIOS <akurbatov.work@gmail.com>
2022-11-04 19:29:03 +0300

    Initial Commit

---
+++ States Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  States Quiz
+//
+//  Created by Kurbatov Artem on 04.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
