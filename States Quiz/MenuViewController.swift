37f7c168a7d9637099f72f3343ed7e19eb43cb67
KurbatovIOS <akurbatov.work@gmail.com>
2022-11-07 22:39:20 +0300

    changed game view

--- States Quiz/MenuViewController.swift
+++ States Quiz/MenuViewController.swift
@@ -10,57 +10,80 @@ import SnapKit
 
 class MenuViewController: UIViewController {
     
-    private let gameCollectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-        return collectionView
-    }()
+//    private let gameCollectionView: UICollectionView = {
+//        let layout = UICollectionViewFlowLayout()
+//        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+//        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+//        return collectionView
+//    }()
+    
+    private var playButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
         
-        setUpCollection()
+        //setUpCollection()
         
-        gameCollectionView.delegate = self
-        gameCollectionView.dataSource = self
+       // gameCollectionView.delegate = self
+       // gameCollectionView.dataSource = self
+        
+        setUpButton()
     }
         
     private func setUpCollection() {
         
-        view.addSubview(gameCollectionView)
-        gameCollectionView.backgroundColor = .systemBackground
-        
-        gameCollectionView.snp.makeConstraints { make in
-            make.top.leading.equalToSuperview().offset(20)
-            make.bottom.trailing.equalToSuperview().offset(-20)
-        }
+//        view.addSubview(gameCollectionView)
+//        gameCollectionView.backgroundColor = .systemBackground
+//
+//        gameCollectionView.snp.makeConstraints { make in
+//            make.top.leading.equalToSuperview().offset(20)
+//            make.bottom.trailing.equalToSuperview().offset(-20)
+//        }
     }
     
-    private func setUpViews() {
+    private func setUpButton() {
         
+        view.addSubview(playButton)
         
+        playButton.setImage(UIImage(systemName: "play.circle"), for: .normal)
+        playButton.imageView?.tintColor = .systemGreen
+
+        playButton.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
+        
+        playButton.imageView?.snp.makeConstraints({ make in
+            make.height.width.equalTo(100)
+        })
+        
+        playButton.addTarget(self, action: #selector(playButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func playButtonTapped() {
+        print("AY")
     }
 }
 
-extension MenuViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = gameCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .purple
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let size = collectionView.frame.width * 0.45
-        return CGSize(width: size, height: size)
-    }
-}
+
+
+//extension MenuViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+//
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 2
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//
+//        let cell = gameCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+//        cell.backgroundColor = .purple
+//        return cell
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//
+//        let size = collectionView.frame.width * 0.45
+//        return CGSize(width: size, height: size)
+//    }
+//}
