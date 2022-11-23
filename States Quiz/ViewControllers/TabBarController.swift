908a8bed633348df856233314d9db2c120731067
KurbatovIOS <akurbatov.work@gmail.com>
2022-11-23 19:31:32 +0300

    game history screen addition

--- States Quiz/ViewControllers/TabBarController.swift
+++ States Quiz/ViewControllers/TabBarController.swift
@@ -9,18 +9,23 @@ import UIKit
 
 class TabBarController: UITabBarController {
 
+    
+    #warning("Change icons")
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let menuVC = UINavigationController(rootViewController: MenuViewController())
-        let settingsVC = UINavigationController(rootViewController: SettingsViewController())
+       // let settingsVC = UINavigationController(rootViewController: SettingsViewController())
+        let historyVC = UINavigationController(rootViewController: GameViewController())
                 
         menuVC.tabBarItem.image = UIImage(systemName: "list.bullet")
         menuVC.tabBarItem.title = "Games"
-        settingsVC.tabBarItem.image = UIImage(systemName: "gear")
-        settingsVC.tabBarItem.title = "Settings"
+        //settingsVC.tabBarItem.image = UIImage(systemName: "gear")
+        //settingsVC.tabBarItem.title = "Settings"
+        historyVC.tabBarItem.image = UIImage(systemName: "list.star")
+        historyVC.tabBarItem.title = "History"
         
-        setViewControllers([menuVC, settingsVC], animated: true)
+        setViewControllers([menuVC, historyVC], animated: true)
     }
 }
 
